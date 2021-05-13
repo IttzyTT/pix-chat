43eaca9732bba324d43bea70de6ab26148710fb9
Bryan Larsson <bryanlarsson99@hotmail.com>
2021-05-13 12:36:15 +0200

    test

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react'
+import Topbar from './components/Topbar'
 
 function App() {
 
   return (
     <div className="App">
-     
+      
     </div>
   )
 }
---
+++ frontend/src/components/Topbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import logo from '/logo.png';
+
+function topbar() {
+    return (
+        <div>
+            <img src={logo} />
+        </div>
+    )
+}
+
+export default topbar
