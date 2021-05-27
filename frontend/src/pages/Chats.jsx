0403d3fdfd552c9040aefe735cc60fbe320d96e6
Kevin Leskinen <kevnl_95@hotmail.com>
2021-05-27 14:12:13 +0200

    showing messages in chats page

--- frontend/src/pages/Chats.jsx
+++ frontend/src/pages/Chats.jsx
@@ -1,6 +1,74 @@
 import React, { useEffect, useState } from 'react';
-import styled from 'styled-components'
+import styled from 'styled-components';
+import { useNamedContext } from 'react-easier';
 
+
+function Chats() {
+    const [post, setPost] = useState ({});
+    const [messages, setMessage] = useState ([]);
+    let globalStore = useNamedContext('global');
+
+
+    useEffect(() => {
+       fetchAllPosts();
+       fetchAllMessages();
+    }, [])
+
+    const fetchAllMessages = async () => {
+       
+        try {
+            const response = await fetch(`http://localhost:4000/postMessages`);
+            const data = await response.json(); 
+            console.log(data['createdAt']);
+            setMessage(data);
+            } catch (error) {
+                console.log(error)
+            }
+    }
+
+    const fetchAllPosts = async () => {
+
+        try {
+            const response = await fetch(`http://localhost:4000/posts`);
+            const data = await response.json(); 
+            console.log(data.createdAt);
+            setPost(data);
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    const filterMessages = (messages) => {
+        return(
+            messages.filter(message => (
+                message.createdById === globalStore.currentUserId
+            ))
+        ) 
+    }
+
+    return (
+        <Wrapper>
+            <Title>Chats</Title>
+            <div>
+                {filterMessages(messages).map((message) => {
+                    let createdAt = new window.Date(message.createdAt).toLocaleDateString('sv-SE');
+
+                    return (
+                        <div key={message['_id']}>
+                            <Date>{createdAt}</Date>
+                            <Caption>{message.content}</Caption>
+                        </div>
+                    )
+                    })
+                }
+            </div>
+        </Wrapper>
+        
+        
+    )
+}
+
+export default Chats
 
 const Wrapper = styled.div`
 padding-top: 100px;
@@ -14,54 +82,3 @@ color: #FFFF;
 const Caption = styled.h2`
 color: #5590e7;
 `
-
-
-function Chats() {
-    const [user, setUser] = useState ({})
-    const [messages, setMessage] = useState ([])
-
-    let createdAt = new window.Date(post.createdAt).toLocaleDateString();
-
-    useEffect(() => {
-       fetchAllUsers
-       fetchAllPosts
-    }, [])
-
-    const fetchAllPosts = async () => {
-       
-        try {
-                const response = await fetch(`http://localhost:4000/postsMessages`);
-                const data = await response.json(); 
-                console.log(data)
-            } catch (error) {
-                console.log(error)
-        }
-    }
-
-    const fetchAllUsers = async () => {
-
-        try {
-            const response = await fetch(`http://localhost:4000/users`);
-            const data = await response.json(); 
-            console.log(data)
-        } catch (error) {
-            console.log(error)
-        }
-    }
-
-    return (
-        <Wrapper>
-            <Title>Chats</Title>
-                <Date>{createdAt}</Date>
-                    <Caption>{post.caption}</Caption>
-                    
-                    <MessageList>
-                        <p> Her comes a message list!</p>
-                    </MessageList>
-        </Wrapper>
-        
-        
-    )
-}
-
-export default Chats
