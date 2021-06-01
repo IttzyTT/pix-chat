c95809576d5ce3f93ce89de6d0b2e922f0d38e26
Oscar Lundberg <oscarlundberg@hotmail.com>
2021-06-01 11:10:31 +0200

    moved mongoDB url to gitignored file

--- api/api.js
+++ api/api.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const cors = require("cors");
 const sse = require('./SSE-handler');
+const mongoDBUrl = require('./settings/dontCommit.json').mongoDBUrl;
 
 app.use(cors());
 app.use(express.json({limit: '50mb'}));
@@ -22,7 +23,7 @@ sse.sse(app);
 
 // Connect to Mongo DB Atlas
 mongoose.connect(
-    "mongodb+srv://pixchat:frk20s@cluster0.qmhfd.mongodb.net/pixChat?retryWrites=true&w=majority",
+    mongoDBUrl,
     { useNewUrlParser: true, useUnifiedTopology: true },
     () => {
         console.log('DB connected');
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -79,7 +79,7 @@ export default function Login() {
             createAccountHandler();
         }
     }
-    console.log(currentPage);
+
     return (
         <Flexwrapper>
             <Centering>
